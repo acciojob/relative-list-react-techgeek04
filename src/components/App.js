83db52596dc3c81cc6ebaa6fb0d2a84048321e17
techgeek04 <155185231+techgeek04@users.noreply.github.com>
2025-04-09 14:55:38 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
 const App = () => {
+  const names = ["A","B","C","D"]
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+               <ol key={"relativeList"}>
+    {
+      names.map((i,index)=>(<li key={`relativeListItem${index}`}>{i}</li>))
+    }
+              </ol>
     </div>
   )
 }
